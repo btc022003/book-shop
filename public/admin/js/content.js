1f57180fd5eea34c60826fd28d6ce17a42f41e7d
arivin <btc022003@gmail.com>
2017-01-16 16:01:07 +0800

    添加自定义验证插件

--- public/admin/js/content.js
+++ public/admin/js/content.js
@@ -54,38 +54,43 @@ function WinMove() {
 
 //默认的验证插件配置信息
 $.validator.setDefaults({
-    errorElement: "em",
-    errorPlacement: function(error, element) {
-        // Add the `help-block` class to the error element
-        error.addClass("help-block");
+        errorElement: "em",
+        errorPlacement: function(error, element) {
+            // Add the `help-block` class to the error element
+            error.addClass("help-block");
 
-        // Add `has-feedback` class to the parent div.form-group
-        // in order to add icons to inputs
-        element.parents(".col-sm-10").addClass("has-feedback");
+            // Add `has-feedback` class to the parent div.form-group
+            // in order to add icons to inputs
+            element.parents(".col-sm-10").addClass("has-feedback");
 
-        if (element.prop("type") === "checkbox") {
-            error.insertAfter(element.parent("label"));
-        } else {
-            error.insertAfter(element);
-        }
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.parent("label"));
+            } else {
+                error.insertAfter(element);
+            }
 
-        // Add the span element, if doesn't exists, and apply the icon classes to it.
-        if (!element.next("span")[0]) {
-            $("<span class='glyphicon glyphicon-remove form-control-feedback'></span>").insertAfter(element);
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if (!element.next("span")[0]) {
+                $("<span class='glyphicon glyphicon-remove form-control-feedback'></span>").insertAfter(element);
+            }
+        },
+        success: function(label, element) {
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if (!$(element).next("span")[0]) {
+                $("<span class='glyphicon glyphicon-ok form-control-feedback'></span>").insertAfter($(element));
+            }
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-error").removeClass("has-success");
+            $(element).next("span").addClass("glyphicon-remove").removeClass("glyphicon-ok");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");
+            $(element).next("span").addClass("glyphicon-ok").removeClass("glyphicon-remove");
         }
-    },
-    success: function(label, element) {
-        // Add the span element, if doesn't exists, and apply the icon classes to it.
-        if (!$(element).next("span")[0]) {
-            $("<span class='glyphicon glyphicon-ok form-control-feedback'></span>").insertAfter($(element));
-        }
-    },
-    highlight: function(element, errorClass, validClass) {
-        $(element).parents(".col-sm-10").addClass("has-error").removeClass("has-success");
-        $(element).next("span").addClass("glyphicon-remove").removeClass("glyphicon-ok");
-    },
-    unhighlight: function(element, errorClass, validClass) {
-        $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");
-        $(element).next("span").addClass("glyphicon-ok").removeClass("glyphicon-remove");
-    }
+    })
+//添加手机号码验证
+$.validator.addMethod('isMobile', function(val, ele) {
+    var regMobile = /^1[3857]{1}\d{9}$/
+    return regMobile.test(val)
 })
