0053ae8f83e03c0541f5ad7c1296d0b2d3f69ac0
arivin <btc022003@gmail.com>
2017-02-14 17:30:12 +0800

    修改服务器端返回数据的格式
    使用rest风格重新处理服务器端返回数据的格式,去掉最外层的返回数据包括

--- routes/api/v1/books.js
+++ routes/api/v1/books.js
@@ -4,7 +4,7 @@ var BookDal = require('../../../common/book').BookDal
 var bookDal = new BookDal()
 
 router.get('/get_data',(req,res)=>{
-	bookDal.getData({type:"ertong"},books=>{
+	bookDal.getData({type:"other"},books=>{
 		res.json({
 			status:'y',
 			msg:'请求成功',
@@ -19,13 +19,22 @@ router.get('/get_data',(req,res)=>{
 	// 	})
 	// },5000)
 })
+//无条件的返回数据
+router.get('/',(req,res)=>{
+	bookDal.getData({type:'other'},data=>{
+		// res.json({status:'y',msg:'获取数据成功',data:data})
+		res.json(data)
+	})
+})
+
 //根据id获取单条记录
 // /api/v1/books/....
 router.get('/:id',(req,res)=>{
 	bookDal.findByID(req.params.id,data=>{
 		// data = data.toJSON()
 		// delete data.__v
-		res.json({status:'y',msg:'获取数据成功',data:data})
+		// res.json({status:'y',msg:'获取数据成功',data:data})
+		res.json(data)
 	})
 })
 //根据id更新一条记录
@@ -34,9 +43,10 @@ router.put('/:id',(req,res)=>{
 	delete req.body._id //删除不需要修改的数据
 	delete req.body.__v	//删除不需要修改的数据
 	bookDal.updateByID(req.params.id,req.body,isOk=>{
-		if(isOk){
-			res.json({status:'y',msg:'修改数据成功',data:req.body})
-		}
+		// if(isOk){
+		// 	res.json({status:'y',msg:'修改数据成功',data:req.body})
+		// }
+		res.json(req.body)
 	})
 })
 //新增记录
@@ -49,14 +59,16 @@ router.post('/',(req,res)=>{
 	}
 	bookDal.save(req.body,(isOK,data)=>{
 		if(isOK){
-			res.json({status:'y',msg:'新增数据成功',data:data})
+			// res.json({status:'y',msg:'新增数据成功',data:data})
+			res.json(data)
 		}
 	})
 })
 //根据id删除记录
 router.delete('/:id',(req,res)=>{
 	bookDal.del(req.params.id,isOK=>{
-		res.json({status:'y',msg:'删除数据成功',data:{}})
+		// res.json({status:'y',msg:'删除数据成功',data:{}})
+		res.json({})
 	})
 })
 module.exports = router
