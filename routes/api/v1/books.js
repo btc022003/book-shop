8f77267060f9d62d85afe2caf8e4102f4b53af39
arivin <btc022003@gmail.com>
2017-02-18 09:00:40 +0800

    修改接口部分返回数据的方式

--- routes/api/v1/books.js
+++ routes/api/v1/books.js
@@ -3,22 +3,6 @@ var router = express.Router()
 var BookDal = require('../../../common/book').BookDal
 var bookDal = new BookDal()
 
-router.get('/get_data',(req,res)=>{
-    bookDal.getData({type:"other"},books=>{
-        res.json({
-            status:'y',
-            msg:'请求成功',
-            data:books
-        })
-    })
-    // setTimeout(function(){
-    //  res.json({
-    //      status:'y',
-    //      msg:'请求成功',
-    //      data:[]
-    // })
-    // },5000)
-})
 //无条件的返回数据
 router.get('/',(req,res)=>{
     //bookDal.getData({type:req.query.type},data=>{
