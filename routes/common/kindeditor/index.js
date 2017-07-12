32654d7518fd4b024bdedc3adae80a9c2e931492
arivin <btc022003@gmail.com>
2017-07-12 08:57:32 +0800

    添加kindeditor上传图片插件的安装

--- routes/common/kindeditor/index.js
+++ routes/common/kindeditor/index.js
@@ -3,32 +3,32 @@
  */
 var express = require('express');
 var router = express.Router();
-//var formidable = require('formidable');
+var formidable = require('formidable');
 
 
 
-//router.post('/uploadImg', function(req, res, next) {
-//    var form = new formidable.IncomingForm();
-//    form.keepExtensions = true;
-//    //注意此处上传目录的路径
-//    form.uploadDir = __dirname+'/../../../public/upload';
+router.post('/uploadImg', function(req, res, next) {
+    var form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    //注意此处上传目录的路径
+    form.uploadDir = __dirname+'/../../../public/upload';
 
-//    form.parse(req, function (err, fields, files) {
-//        if (err) {
-//            throw err;
-//        }
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            throw err;
+        }
 
-//        var image = files.imgFile;
-//        var path = image.path;
-//        path = path.replace(/\\/g, '/'); //正则替换反斜杠
-//        var url = '/upload' + path.substr(path.lastIndexOf('/'), path.length);
-//        console.log(url)
-//        var info = {
-//            "error": 0,
-//            "url": url
-//        };
-//        res.send(info);
-//    });
-//});
+        var image = files.imgFile;
+        var path = image.path;
+        path = path.replace(/\\/g, '/'); //正则替换反斜杠
+        var url = '/upload' + path.substr(path.lastIndexOf('/'), path.length);
+        console.log(url)
+        var info = {
+            "error": 0,
+            "url": url
+        };
+        res.send(info);
+    });
+});
 
 module.exports = router;
